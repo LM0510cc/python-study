81b5b7b9e1c7fcc6f92276d3b5b8152439be4aa4
LM0510cc <1765684136@qq.com>
2026-09-18 12:58:00 +0800

    添加BNI计算器，英语单词记忆小程序，python基础代码练习

---
+++ BMI calculation.py
@@ -0,0 +1,20 @@
+while True:
+    weight_input = input("请输入您的体重 单位 kg：")
+
+    if weight_input == "所有人已计算完毕":
+        print("\33[91m" + "好的，BMI计算程序已关闭 "+ "\33[0m")
+        break
+    user_weight = float(weight_input)
+    user_height = float(input("请输入您的身高 单位 m:"))
+    BMI = user_weight / (user_height)**2
+    print("您的BMI为\033[94m" + str(BMI) + "\033[0m")
+    if BMI < 18.5:
+        print("太瘦了，兄弟，该增肌了")
+    elif BMI < 24:
+        print("牛逼兄弟，身材真是完美\n同龄男生看了流泪")
+    elif BMI < 28:
+        print("兄弟，稍微有点胖了，晚上尽量别吃饭了！")
+    else:
+        print("逼自己一把，你一定会发现更好的自己，该减肥了兄弟")
+
+
---
+++ vocabulary_memorizatiaon.py
@@ -0,0 +1,19 @@
+vocabulary_dict = {"company":"公司，交往，陪伴","action":"行为，行动"}
+vocabulary_dict["balance"] = "保持平衡，平衡，抵消"
+vocabulary_dict["stretch"] = "伸展，延伸，夸大"
+vocabulary_dict["diet"] = "日常饮食，节食"
+vocabulary_dict["frame"] = "画框，构架，画面"
+
+
+
+while True:
+    learn_english = input("请输入您要默写的单词，并默念它的以意思（结束请说‘今天的任务已完成’）：")
+    if learn_english == "今天的任务已完成":
+        print("我就知道你一定能行！！！加油，明天我们不见不散")
+        print("当前英语记忆代码总共收录了\033[31m" + str(len(vocabulary_dict)) + "\033[0m" + "个单词\n期待你更多的积累")
+        break
+    if learn_english in vocabulary_dict:
+        print("您查询的英文单词释义为\033[34m" + vocabulary_dict[learn_english] + "\033[0m")
+        print("非常棒，拼写的一点没错，来继续")
+    else:
+        print("再回去翻翻，输入错了，别气馁")
